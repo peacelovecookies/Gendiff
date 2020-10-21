83e92aedafa3ba97ed76bd189b1dffc374c70464
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-21 09:10:21 +0500

    add command and option -f

--- src/bin/index.js
+++ src/bin/index.js
@@ -4,6 +4,8 @@ import { program } from 'commander';
 import { version } from '../../package.json';
 
 program
-  .version(version)
+  .command('gendiff <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .version(version)
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv)

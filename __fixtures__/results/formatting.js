d5cde55fafcd2dad9b4fa18fc32ad6b00e3cf013
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-24 03:12:24 +0500

    add parsing and formatting results

---
+++ __fixtures__/results/formatting.js
@@ -0,0 +1,56 @@
+import { ast } from './parsing';
+
+const stringFormat = `
+{
+    but: {
+        need: {
+      - to: know everything?
+      + to: be worried?
+        }
+        - why: do you
+        + why: am I
+    }
+    - could: [read, this]
+    - d: 1
+    + d: 8
+    - i: do not mind
+    + i: do mind
+      if: you
+    - just: beat it!
+    + just: want to beat it!
+    - my: job is
+    + would: [read, this, message]
+    + you: job is
+    - zapp: {
+        brannigan: leela
+    }
+    - zoidberg: {
+        wants: {
+            to: eat
+        }
+    }
+    + zoidberg: the doctor
+}
+`;
+
+const jsonFormat = JSON.stringify(ast);
+
+const plainFormat = `
+Property 'but.need.to' was updated. From 'know everything?' to 'be worried'
+Property 'but.why' was updated. From 'do you' to 'am I'
+Property 'could' was removed
+Property 'd' was updated. From 1 to 8
+Property 'i' was updated. From 'do not mind' to 'do mind'
+Property 'just' was updated. From 'beat it!' to 'want to beat it!'
+Property 'my' was removed
+Property 'would' was added with value: [complex value]
+Property 'you' was added with value: 'job is'
+Property 'zapp' was removed
+Property 'zoidberg' was updated. From [complex value] to 'the doctor'
+`;
+
+export {
+  stringFormat,
+  plainFormat,
+  jsonFormat,
+};
---
+++ __fixtures__/results/parsing.js
@@ -0,0 +1,114 @@
+const parsedObj = {
+  i: 'do not mind',
+  if: 'you',
+  could: ['read', 'this'],
+  but: {
+    why: 'do you',
+    need: {
+      to: 'know everything?',
+    },
+  },
+  just: 'beat it!',
+  my: 'job is',
+  d: 1,
+  zapp: {
+    brannigan: 'leela',
+  },
+  zoidberg: {
+    wants: {
+      to: 'eat',
+    },
+  },
+};
+
+const ast = [
+  {
+    key: 'but',
+    type: 'nested',
+    children: [
+      {
+        key: 'need',
+        type: 'nested',
+        children: [
+          {
+            key: 'to',
+            type: 'changed',
+            oldValue: 'know everything?',
+            newValue: 'be worried?',
+          },
+        ],
+      },
+      {
+        key: 'why',
+        type: 'changed',
+        oldValue: 'do you',
+        newValue: 'am I',
+      },
+    ],
+  },
+  {
+    key: 'could',
+    type: 'deleted',
+    oldValue: ['read', 'this'],
+  },
+  {
+    key: 'd',
+    type: 'changed',
+    oldValue: 1,
+    newValue: 8,
+  },
+  {
+    key: 'i',
+    type: 'changed',
+    oldValue: 'do not mind',
+    newValue: 'do mind',
+  },
+  {
+    key: 'if',
+    type: 'unchanged',
+    value: 'you',
+  },
+  {
+    key: 'just',
+    type: 'changed',
+    oldValue: 'beat it!',
+    newValue: 'want to beat it!',
+  },
+  {
+    key: 'my',
+    type: 'deleted',
+    oldValue: 'job is',
+  },
+  {
+    key: 'would',
+    type: 'added',
+    newValue: ['read', 'this', 'message'],
+  },
+  {
+    key: 'your',
+    type: 'added',
+    newValue: 'job is',
+  },
+  {
+    key: 'zapp',
+    type: 'deleted',
+    oldValue: {
+      brannigan: 'leela',
+    },
+  },
+  {
+    key: 'zoidberg',
+    type: 'changed',
+    oldValue: {
+      wants: {
+        to: 'eat',
+      },
+    },
+    newValue: 'the doctor',
+  },
+];
+
+export {
+  parsedObj,
+  ast,
+};

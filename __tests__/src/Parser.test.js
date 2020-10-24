810da0a9d88f6ed4472a7e72ce261f6a67550ca2
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-24 13:33:16 +0500

    add Parser test

---
+++ __tests__/src/Parser.test.js
@@ -0,0 +1,36 @@
+import { describe, expect, test } from '@jest/globals';
+
+import path from 'path';
+import _ from 'lodash';
+
+import { fileURLToPath } from 'url';
+
+import Parser from '../../src/Parser';
+import { parsedObj, ast as resultAST } from '../../__fixtures__/results/parsing';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+describe('Parsing tests', () => {
+  const differentFormatsCase = [
+    ['.json', parsedObj],
+    ['.yaml', parsedObj],
+    ['.yml', parsedObj],
+  ];
+
+  test.each(differentFormatsCase)('%p format', (ext, result) => {
+    const filepath = path.resolve(__dirname, '../..', '__fixtures__', `before${ext}`);
+    const parsed = Parser.parse(filepath);
+    expect(parsed).toEqual(result);
+  });
+
+  test.only('pasring ast', () => {
+    const filepath1 = path.resolve(__dirname, '../..', '__fixtures__', 'before.json');
+    const filepath2 = path.resolve('__fixtures__/after.json');
+    const parsed1 = Parser.parse(filepath1);
+    const parsed2 = Parser.parse(filepath2);
+
+    const ast = Parser.getAST(parsed1, parsed2);
+    expect(ast).toEqual(resultAST);
+  });
+});

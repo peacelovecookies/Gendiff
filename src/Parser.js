5eb22a189523ad386b8112f027487c1e6b9ffcdc
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-26 21:36:04 +0500

    add error when used unsupported ext + change logic

--- src/Parser.js
+++ src/Parser.js
@@ -2,30 +2,23 @@ import YAML from 'yaml';
 import fs from 'fs';
 import path from 'path';
 
-import { fileURLToPath } from 'url';
-
 import _ from 'lodash';
-import isObject from './utils';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import isObject from './utils.js';
 
 export default class Parser {
-  static getValue(key, obj) {
-    const value = obj[key];
-    return isObject(value) ? this.getAST(value, value) : value;
-  }
-
   static getMeta(key, [obj1, obj2]) {
-    const oldValue = this.getValue(key, obj1);
-    const newValue = this.getValue(key, obj2);
+    const oldValue = obj1[key];
+    const newValue = obj2[key];
     if (!_.has(obj2, key)) {
       return { key, type: 'deleted', oldValue };
     }
     if (!_.has(obj1, key)) {
       return { key, type: 'added', newValue };
     }
-    if (obj1[key] === obj2[key]) {
+    if (isObject(oldValue) && isObject(newValue)) {
+      return { key, type: 'nested', children: this.getAST(oldValue, newValue) };
+    }
+    if (_.isEqual(oldValue, newValue)) {
       return { key, type: 'unchanged', value: oldValue };
     }
     // eslint-disable-next-line object-curly-newline
@@ -35,19 +28,11 @@ export default class Parser {
   static getAST(fileBefore, fileAfter) {
     const iter = (...objects) => {
       const clone = _.cloneDeep(objects);
-      const [obj1, obj2] = objects;
       const merged = _.merge(...clone);
       const keys = Object.keys(merged);
       return keys
         .sort()
-        .flatMap((key) => {
-          const valueBefore = obj1[key];
-          const valueAfter = obj2[key];
-          if (isObject(valueBefore) && isObject(valueAfter)) {
-            return { key, type: 'nested', children: iter(valueBefore, valueAfter) };
-          }
-          return this.getMeta(key, objects);
-        });
+        .map((key) => this.getMeta(key, objects));
     };
 
     return iter(fileBefore, fileAfter);
@@ -60,10 +45,19 @@ export default class Parser {
   };
 
   static parse(filepath) {
-    const fullpath = path.resolve(__dirname, filepath);
-    const file = fs.readFileSync(fullpath, 'utf-8');
+    const currentDir = process.cwd();
+    const fullpath = path.resolve(currentDir, filepath);
     const ext = path.extname(filepath);
     const parser = this.parsers[ext];
+
+    if (!parser) {
+      const exts = Object.keys(Parser.parsers).join(', ');
+      const err = new Error();
+      err.name = 'UNSUPPORTED_EXTENSION';
+      err.message = `Currently we support only ${exts}`;
+      throw err;
+    }
+    const file = fs.readFileSync(fullpath, 'utf-8');
 
     return parser(file);
   }

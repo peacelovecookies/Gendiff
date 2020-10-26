26398dcd146bbf596daefab62615bb5b703fbf4c
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-26 21:28:27 +0500

    fix identation, add zun key

--- __fixtures__/results/formatting.js
+++ __fixtures__/results/formatting.js
@@ -4,49 +4,53 @@ const stringFormat = `
 {
     but: {
         need: {
-      - to: know everything?
-      + to: be worried?
+          - to: know everything?
+          + to: be worried?
         }
-        - why: do you
-        + why: am I
+      - why: do you
+      + why: am I
     }
-    - could: [read, this]
-    - d: 1
-    + d: 8
-    - i: do not mind
-    + i: do mind
-      if: you
-    - just: beat it!
-    + just: want to beat it!
-    - my: job is
-    + would: [read, this, message]
-    + you: job is
-    - zapp: {
+  - could: [read, this]
+  - d: 1
+  + d: 8
+  - i: do not mind
+  + i: do mind
+    if: you
+  - just: beat it!
+  + just: want to beat it!
+  - my: job is
+  + would: [read, this, message]
+  + your: job is
+  - zapp: {
         brannigan: leela
     }
-    - zoidberg: {
+  - zoidberg: {
         wants: {
             to: eat
         }
     }
-    + zoidberg: the doctor
+  + zoidberg: the doctor
+  + zun: {
+        ha: hahaha
+    }
 }
 `;
 
 const jsonFormat = JSON.stringify(ast);
 
 const plainFormat = `
-Property 'but.need.to' was updated. From 'know everything?' to 'be worried'
+Property 'but.need.to' was updated. From 'know everything?' to 'be worried?'
 Property 'but.why' was updated. From 'do you' to 'am I'
 Property 'could' was removed
 Property 'd' was updated. From 1 to 8
 Property 'i' was updated. From 'do not mind' to 'do mind'
 Property 'just' was updated. From 'beat it!' to 'want to beat it!'
 Property 'my' was removed
-Property 'would' was added with value: [complex value]
-Property 'you' was added with value: 'job is'
+Property 'would' was added with value: [read, this, message]
+Property 'your' was added with value: 'job is'
 Property 'zapp' was removed
 Property 'zoidberg' was updated. From [complex value] to 'the doctor'
+Property 'zun' was added with value: [complex value]
 `;
 
 export {

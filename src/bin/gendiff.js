8d231e2bbcfb2e7ff1b92789d77e3bbd64a9c6f1
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-27 14:43:15 +0500

    add options -r, --replacer and -s, --spaces for replacer and spaces count

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-//import pkg from '../../package';
+//import pkg from '../../package'; # TODO: know how to import version from package.json
 
 import gendiff from '../gendiff.js';
 
@@ -11,12 +11,19 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'pretty')
+  .option('-f, --format <type>', 'output format, by default "pretty"', 'pretty')
+  .option('-r, --replacer <type>', 'replacer, which used in pretty format, by default " " (one space)', ' ')
+  .option('-s, --spaces <number>', 'count of spaces in pretty format, by default 4', 4)
   .action((filepath1, filepath2, cmdObj) => {
+    const { format, replacer, spaces } = cmdObj;
     const result = gendiff(
       filepath1,
       filepath2,
-      { format: cmdObj.format },
+      {
+        format,
+        replacer,
+        spacesCount: spaces,
+      },
     );
 
     console.log(`\n${result}`);

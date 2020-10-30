67ce4ea17b57e3e3f9adec565a13b645dbf10087
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-30 15:23:49 +0500

    add dynamic version display

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-// import pkg from '../../package'; # TODO: know how to import version from package.json
+import pkg from '../../package.json';
 
 import gendiff from '../gendiff.js';
 
 const program = new Command();
 
 program
-  .version('0.0.1')
+  .version(pkg.version)
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format, by default "pretty"', 'pretty')

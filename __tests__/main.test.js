54f3302555eba9be1ec0545a96d2944e0adbbf7b
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-30 15:25:39 +0500

    re-write tests (what the heck was that, man? You just coppied Formatting test, seriously?)

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,34 +1,26 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 
 import path from 'path';
 
-import Parser from '../src/Parser';
-import Formatter from '../src/Formatter';
-import { stringFormat, jsonFormat, plainFormat } from '../__fixtures__/results/formatting';
+import gendiff from '..';
+import { prettyFormat, jsonFormat, plainFormat } from '../__fixtures__/results/formatting';
 
 describe('main (entering point) checking', () => {
-  const filepath1 = path.resolve(__dirname, '..', '__fixtures__', 'before.json');
-  const filepath2 = path.resolve(__dirname, '..', '__fixtures__', 'after.json');
-  const file1 = Parser.parse(filepath1);
-  const file2 = Parser.parse(filepath2);
-  const parser = new Parser();
-  const ast = parser.getAST(file1, file2);
+  const filepath1 = path.resolve(__dirname, '..', '__fixtures__', 'before.yaml');
+  const filepath2 = path.resolve(__dirname, '..', '__fixtures__', 'after.yaml');
 
-  test('wrong format', () => {
-    const formatter = new Formatter({ format: 'wrooooong' });
-    const formats = formatter.formatters.join(', ');
-    expect(() => formatter.stringify(ast)).toThrow(`Use one of the following formats: ${formats}`);
-  });
-  const differentFormatsCase = [
-    ['pretty', stringFormat],
-    ['json', jsonFormat],
-    ['plain', plainFormat],
+  const prettyOpt = { };
+  const jsonOpt = { format: 'json' };
+  const plainOpt = { format: 'plain' };
+
+  const formatsCase = [
+    [prettyOpt, prettyFormat],
+    [jsonOpt, jsonFormat],
+    [plainOpt, plainFormat],
   ];
 
-  test.each(differentFormatsCase)('%p format', (format, result) => {
-    const formatter = new Formatter({ format });
-    const formatted = formatter.stringify(ast);
-
-    expect(formatted).toEqual(result.trim());
+  it.each(formatsCase)('should find diff', (options, rightResult) => {
+    const result = gendiff(filepath1, filepath2, options);
+    expect(result).toEqual(rightResult.trim());
   });
 });
